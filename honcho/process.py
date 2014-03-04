0705c41924073e0415254576d97b0d729cbc7fc1
kimballfrank <kimballfrank@hotmail.com>
2014-03-03 17:00:54 -0700

    debugging

--- honcho/process.py
+++ honcho/process.py
@@ -173,6 +173,7 @@ class ProcessManager(object):
         self._terminating = True
 
         print("sending SIGTERM to all processes", file=self.system_printer)
+        import pdb; pdb.set_trace()
         for proc in self.processes:
             if proc.poll() is None:
                 print("sending SIGTERM to pid {0:d}".format(proc.pid), file=self.system_printer)
